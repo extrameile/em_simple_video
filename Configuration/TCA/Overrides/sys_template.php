2c47a275f775e5464acb93d33f6bfb1e1d1f1919
Andreas Kiessling <kiessling@extrameile-gehen.de>
2019-11-06 23:17:42 +0100

    configure rendering for css_styled_content

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -8,3 +8,11 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('fluid_styled_c
         'Simple Video Rendering (Fluid Styled Content)'
     );
 }
+
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('css_styled_content')) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        'em_simple_video',
+        'Configuration/TypoScriptCsc/',
+        'Simple Video Rendering (CSS Styled Content)'
+    );
+}
