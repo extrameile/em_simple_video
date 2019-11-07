cb4648c6d2dcbd6d88668db6b66ff60c163801ce
Andreas Kiessling <kiessling@extrameile-gehen.de>
2019-11-07 13:50:31 +0100

    allow installation in TYPO3 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '8.7.13-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
